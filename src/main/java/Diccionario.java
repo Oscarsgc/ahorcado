c7e3d7193008aa47f27105b4b6834b822d320ad0
unknown <DELL@.(none)>
2013-11-18 15:45:14 -0400

    Palabras Cargadas con grado de dificultad

--- src/main/java/Diccionario.java
+++ src/main/java/Diccionario.java
@@ -43,11 +43,40 @@ public class Diccionario {
 		ListaDePalabras peliculas = new ListaDePalabras();
 		peliculas.agregarPalabra(new Palabra("ToyStory", 1, false));
 		peliculas.agregarPalabra(new Palabra("LaMomia", 1, false));
-		peliculas.agregarPalabra(new Palabra("Argos", 1, false));
-		peliculas.agregarPalabra(new Palabra("3Metrossobreelcielo", 1, false));
+		peliculas.agregarPalabra(new Palabra("Argo", 1, false));
+		peliculas.agregarPalabra(new Palabra("Tarzan", 1, false));
 		peliculas.agregarPalabra(new Palabra("AmericanPie", 1, false));
+		peliculas.agregarPalabra(new Palabra("LosPitufos", 2, false));
+		peliculas.agregarPalabra(new Palabra("Hancok", 2, false));
+		peliculas.agregarPalabra(new Palabra("Precious", 2, false));
+		peliculas.agregarPalabra(new Palabra("ScaryMovie", 2, false));
+		peliculas.agregarPalabra(new Palabra("CasaTiburones", 2, false));
+		peliculas.agregarPalabra(new Palabra("Adventureland", 3, false));
+		peliculas.agregarPalabra(new Palabra("3MetrosSobreElCielo", 3, false));
+		peliculas.agregarPalabra(new Palabra("LaPasionDeCristo", 3, false));
+		peliculas.agregarPalabra(new Palabra("LosJuegosDelHambre", 3, false));
+		peliculas.agregarPalabra(new Palabra("DiaroDeUnaPasion", 3, false));
+		
+		ListaDePalabras seriesTV = new ListaDePalabras();
+		seriesTV.agregarPalabra(new Palabra("Friends",1,false));
+		seriesTV.agregarPalabra(new Palabra("Mom",1,false));
+		seriesTV.agregarPalabra(new Palabra("Arrow",1,false));
+		seriesTV.agregarPalabra(new Palabra("Glee",1,false));
+		seriesTV.agregarPalabra(new Palabra("Hostages",1,false));
+		seriesTV.agregarPalabra(new Palabra("LosSimpson",2,false));
+		seriesTV.agregarPalabra(new Palabra("ModernFamily",2,false));
+		seriesTV.agregarPalabra(new Palabra("Vikingos",2,false));
+		seriesTV.agregarPalabra(new Palabra("Homeland",2,false));
+		seriesTV.agregarPalabra(new Palabra("Supercampeones",2,false));
+		seriesTV.agregarPalabra(new Palabra("TheBigBangTheory",3,false));
+		seriesTV.agregarPalabra(new Palabra("Supernatural",3,false));
+		seriesTV.agregarPalabra(new Palabra("TheFollowing",3,false));
+		seriesTV.agregarPalabra(new Palabra("BreakingBad",3,false));
+		seriesTV.agregarPalabra(new Palabra("AgentsOfShield",3,false));
 		
 		addList(paises);
+		addList(peliculas);
+		addList(seriesTV);
 	}
 
 }
