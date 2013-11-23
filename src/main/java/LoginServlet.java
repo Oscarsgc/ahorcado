31327cf5bbc1cfeb56eb5ea29744da05c005f2e2
PatriciaRosembluth <patty_rosembluth@hotmail.com>
2013-11-23 17:31:48 -0400

    Interfaces y servlets de login y sign up

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class LoginServlet extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+	}
+
+}
